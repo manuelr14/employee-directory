146569fcd8b4bdc78f562c030981d6c013caf305
Manuel Ramirez <manuelramirez1411@gmail.com>
2020-04-22 12:55:32 -0400

    nav styles changed

--- employee-directorymr/src/components/Navbar.js
+++ employee-directorymr/src/components/Navbar.js
@@ -1,11 +1,27 @@
 import React from "react";
 import "../style/style.css";
 
+const styles = {
+    nav: {
+        background: "rgb(9,51,94)",
+        padding: "3px",
+        height:"97px"
+ 
+    },
+    info: {
+        color: " rgb (255,255,255)",
+        fontsize: "24px"
+
+    },
+
+
+  }; 
+
 function Navbar() {
     return (
-        <div className="jumbotron border-danger" id="jumbotron-red" style="background-color: rgb(9,51,94);padding: 3px;height: 97px;">
-            <p className="text-center" style="height: 0px;color: rgb(255,255,255);font-size: 24px;">Employee Directory</p>
-            <p className="text-center" style="height: 0px;color: rgb(255,255,255);padding: 24px;">Click on carrots to filter by headings or use the search box</p>
+        <div className="jumbotron border-danger" id="jumbotron-red" style={styles.nav}>
+            <p className="text-center" style={styles.info}>Employee Directory</p>
+            <p className="text-center" style={styles.info}>Click on carrots to filter by headings or use the search box</p>
         </div>
     )
 };
