d9afd8232fb0673ac23b954a09f38375bd94f2cf
Manuel Ramirez <manuelramirez1411@gmail.com>
2020-04-22 18:01:24 -0400

    api in cosole log

--- employee-directorymr/src/utils/API.js
+++ employee-directorymr/src/utils/API.js
@@ -1,20 +1,49 @@
   
 import axios from 'axios';
 
-
 export default {
 	getRandomEmployee : function() {
 		return axios
-			.get('https://api.github.com/orgs/github/public_members')
+            .get('https://api.github.com/orgs/github/public_members')
 			.then((res) => {
 				const employees = res.data;
 				return employees.map((employee) => {
 					return {
 						login      : employee.login,
-						image      : employees.avatar_url,
-						profileUrl : employee.html_url
+						image      : employee.avatar_url,
+                        profileUrl : employee.html_url
+                        // name  : employee.name.first,
+                        // image : employee.thumbnail,
+                        // email : employee.email,
+                        // phone : employee.phone,
+                        // dob   : employee.dob.date
 					};
 				});
 			});
 	}
 };
+
+// name  : employee.name.first,
+// image : employee.thumbnail,
+// email : employee.email,
+// phone : employee.phone,
+// dob : employee.dob.date
+
+
+
+// https://api.github.com/orgs/github/public_members
+
+  
+// import axios from "axios";
+
+
+
+// export default {
+//   getAllEmployees: function() {
+//     return axios.get("https://randomuser.me/api/?results=200&nat=us&inc=picture,name,phone,email,dob");
+//   },
+
+//  getEmployeeByName: function () {
+//      return axios.get("https://randomuser.me/api/?results=200&nat=us&name");
+//  }
+// };
