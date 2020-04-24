ce9ed2d7511998b8ef41fa2f0694a9576af71b21
Manuel Ramirez <manuelramirez1411@gmail.com>
2020-04-24 14:14:28 -0400

    Updates

---
+++ precache-manifest.ed475678b7cd4c8bc3ab3be8bd73f8f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "220661480ca65e93edfd2ffd6f9596c5",
+    "url": "/employee-directory/employee-directorymr/index.html"
+  },
+  {
+    "revision": "c984e395aff501e033d7",
+    "url": "/employee-directory/employee-directorymr/static/css/main.959096d7.chunk.css"
+  },
+  {
+    "revision": "6fd70b6b1356021e5eb6",
+    "url": "/employee-directory/employee-directorymr/static/js/2.da37a344.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-directory/employee-directorymr/static/js/2.da37a344.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c984e395aff501e033d7",
+    "url": "/employee-directory/employee-directorymr/static/js/main.0860b1df.chunk.js"
+  },
+  {
+    "revision": "32acd7ba57c06f44ff44",
+    "url": "/employee-directory/employee-directorymr/static/js/runtime-main.85607e5e.js"
+  }
+]);
